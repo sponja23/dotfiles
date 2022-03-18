0e7967b69861f47e69ef19c013388d03cb60a966
Sponja- <tomas.spognardi@gmail.com>
2022-03-18 19:18:52 -0300

    Commented zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,8 +3,13 @@
 source ../utils.sh
 
 if test-arch; then
+    # ZSH 
     arch-install zsh
+    
+    # Oh-My-Zsh and plugin dependencies
     aur-install oh-my-zsh autojump fzf
+
+    # Custom plugins
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 elif test-debian; then
@@ -15,6 +20,9 @@ else
     exit 1
 fi
 
+# Configs
 ln -s $PWD/.p10k.zsh $HOME/
 ln -s $PWD/.zshrc $HOME/
+
+# Set default shell
 chsh -s $(which zsh)
