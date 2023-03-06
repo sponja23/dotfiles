2e35c322a0e0bcd69aa6a9d4f7a6af314d7ba4ba
sponja23 <tomas.spognardi@gmail.com>
2023-03-06 04:31:38 -0300

    Fixed debian alacritty installation

--- alacritty/README.md
+++ alacritty/README.md
@@ -3,4 +3,7 @@
 ## Requirements
 
 * [alacritty](https://github.com/alacritty/alacritty).
-* Meslo Nerd Font patched for Powerlevel10k.
+  * [Arch package](https://archlinux.org/packages/community/x86_64/alacritty/).
+  * 
+* [MesloLGS Nerd Font](https://github.com/romkatv/powerlevel10k-media).
+  * [Arch package (AUR)](https://aur.archlinux.org/packages/ttf-meslo-nerd-font-powerlevel10k).
--- alacritty/install.sh
+++ alacritty/install.sh
@@ -12,9 +12,28 @@ else
     if test-arch; then
         arch-install alacritty ttf-meslo-nerd-font-powerlevel10k
     elif test-debian; then
-        debian-install alacritty
+        check_program cargo
+        
+        cargo install alacritty
 
-        echo "MesloLGS Nerd Font must be installed manually on debian" 
+        (
+            # Meslo Nerd Font install
+            # Clone repo into tmp
+            cd /tmp || exit 1
+
+            git clone https://github.com/romkatv/powerlevel10k-media
+
+            # Copy font to local fonts
+            sudo cp powerlevel10k-media/MesloLGS\ NF\ Regular.ttf /usr/share/fonts/MesloLGS-NF-Regular.ttf
+            sudo cp powerlevel10k-media/MesloLGS\ NF\ Bold.ttf /usr/share/fonts/MesloLGS-NF-Bold.ttf
+            sudo cp powerlevel10k-media/MesloLGS\ NF\ Italic.ttf /usr/share/fonts/MesloLGS-NF-Italic.ttf
+            sudo cp powerlevel10k-media/MesloLGS\ NF\ Bold\ Italic.ttf /usr/share/fonts/MesloLGS-NF-BoldItalic.ttf
+
+            # Update font cache
+            sudo fc-cache -f -v
+
+            rm -rf ./powerlevel10k-media
+        )
     else
         echo "Unsupported OS. Please install alacritty manually."
         exit 1
