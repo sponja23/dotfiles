0be70b82b051f1942eefb7f861d316a3751a750e
sponja23 <tomas.spognardi@gmail.com>
2023-03-06 05:16:04 -0300

    Fixed relative path error in install scripts

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ../utils.sh
+source "$(dirname -- "${BASH_SOURCE[0]}")/../utils.sh"
 
 check_program stow
 
@@ -21,31 +21,13 @@ if ! [ -x "$(command -v alacritty)" ] && prompt "alacritty not found. Install it
 fi
 
 # MesloLGS Nerd Font
-if ! [ -f "/usr/share/fonts/MesloLGS-NF-Regular.ttf" ] && prompt "MesloLGS Nerd Font not found. Install it now?"; then
+if ! (fc-list | grep -q MesloLGS-NF) && prompt "MesloLGS Nerd Font not found. Install it now?"; then
     echo "Installing MesloLGS Nerd Font..."
 
     if test-arch; then
         check_program "$AUR_HELPER"
 
         aur-install ttf-meslo-nerd-font-powerlevel10k
-    elif test-debian; then
-        (
-            # Clone repo into tmp
-            cd /tmp || exit 1
-
-            git clone https://github.com/romkatv/powerlevel10k-media
-
-            # Copy font to local fonts
-            sudo cp powerlevel10k-media/MesloLGS\ NF\ Regular.ttf /usr/share/fonts/MesloLGS-NF-Regular.ttf
-            sudo cp powerlevel10k-media/MesloLGS\ NF\ Bold.ttf /usr/share/fonts/MesloLGS-NF-Bold.ttf
-            sudo cp powerlevel10k-media/MesloLGS\ NF\ Italic.ttf /usr/share/fonts/MesloLGS-NF-Italic.ttf
-            sudo cp powerlevel10k-media/MesloLGS\ NF\ Bold\ Italic.ttf /usr/share/fonts/MesloLGS-NF-BoldItalic.ttf
-
-            # Update font cache
-            sudo fc-cache -f -v
-
-            rm -rf ./powerlevel10k-media
-        )
     else
         echo "Unsupported OS. Please install MesloLGS Nerd Font manually."
         exit 1
