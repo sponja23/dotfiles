d34d5719b3257fb153934a002742337e90b5bb05
sponja23 <tomas.spognardi@gmail.com>
2023-03-07 05:15:13 -0300

    Merged setup.sh into install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
+
+set -e
+source ./utils.sh
+
+if test-arch; then
+    arch-install --needed git base-devel curl
+
+    # Install paru if needed
+    if [ -x "$(command -v paru)" ]; then
+        git clone https://aur.archlinux.org/paru.git /tmp/paru
+        (cd /tmp/paru && makepkg -si)
+    fi
+
+    # Install rust if needed
+    if [ -x "$(command -v rustc)" ]; then
+        arch-install rustup
+        rustup default stable
+    fi
+elif test-debian; then
+    debian-install git build-essential curl
+
+    # Install rust if needed
+    if [ -x "$(command -v rustc)" ]; then
+        debian-install rustup
+        rustup default stable
+    fi
+else
+    echo "Not Implemented"
+    exit 1
+fi
+
 # Install packages
 
 packages=(
