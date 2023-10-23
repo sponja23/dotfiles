d4288876addf4cc43623a4dc17c892a19263ab4c
sponja23 <tomas.spognardi@gmail.com>
2023-10-23 14:28:59 -0300

    Added vimrc config and sensible.vim

--- link.sh
+++ link.sh
@@ -7,5 +7,6 @@ link-files --no-folding alacritty
 link-files neovim # Fold nvim config
 link-files --no-folding powerlevel10k
 link-files --no-folding zsh
-link-files --no-folding x
-link-files xmonad # Fold xmonad config
+link-files vim # Fold vim config
+# link-files --no-folding x
+# link-files xmonad # Fold xmonad config
