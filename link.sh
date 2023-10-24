e99a16101bad0ea3fcb10751dab2bfd9df7623a3
sponja23 <tomas.spognardi@gmail.com>
2023-10-24 03:55:20 -0300

    Added polybar config

--- link.sh
+++ link.sh
@@ -9,5 +9,6 @@ link-files --no-folding powerlevel10k
 link-files --no-folding zsh
 link-files vim # Fold vim config
 link-files --no-folding i3
+link-files --no-folding polybar # Fold polybar config
 # link-files --no-folding x
 # link-files xmonad # Fold xmonad config
