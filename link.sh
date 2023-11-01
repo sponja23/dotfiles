702773bf005f5d67f723f901f7bdb4e05ff44128
sponja23 <tomas.spognardi@gmail.com>
2023-11-01 18:46:26 -0300

    Added awesome wm config

--- link.sh
+++ link.sh
@@ -9,6 +9,7 @@ link-files --no-folding powerlevel10k
 link-files --no-folding zsh
 link-files vim # Fold vim config
 link-files --no-folding i3
+link-files --no-folding awesome
 link-files --no-folding polybar # Fold polybar config
 link-files --no-folding picom
 # link-files --no-folding x
