a34ecdca2d10cd141a5f997f614af770762a7f39
sponja23 <tomas.spognardi@gmail.com>
2023-12-27 05:16:18 -0300

    Added NvChad-based nvim config

--- link.sh
+++ link.sh
@@ -4,13 +4,13 @@ source ./utils.sh
 
 # Link files
 link-files --no-folding alacritty
-link-files neovim # Fold nvim config
+link-files nvim # Fold nvim config
 link-files --no-folding powerlevel10k
 link-files --no-folding zsh
 link-files vim # Fold vim config
 link-files --no-folding i3
-link-files --no-folding awesome
-link-files --no-folding polybar # Fold polybar config
+link-files awesome # Fold awesome config
+link-files polybar # Fold polybar config
 link-files --no-folding picom
 # link-files --no-folding x
 # link-files xmonad # Fold xmonad config
