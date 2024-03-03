cc1a52a232bd4e2de23a04743a8566573ec10ad4
sponja23 <tomas.spognardi@gmail.com>
2024-03-03 02:36:55 -0300

    Removed awesome config

--- link.sh
+++ link.sh
@@ -4,12 +4,12 @@ source ./utils.sh
 
 # Link files
 link-files --no-folding alacritty
-link-files nvim # Fold nvim config
+# link-files nvim # Fold nvim config
 link-files --no-folding powerlevel10k
 link-files --no-folding zsh
 link-files vim # Fold vim config
 link-files --no-folding i3
-link-files awesome # Fold awesome config
+# link-files awesome # Fold awesome config
 link-files polybar # Fold polybar config
 link-files --no-folding picom
 # link-files --no-folding x
